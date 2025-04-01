9cf826452aabba4a6c052356bab1817bcb6586e2
telluricaquarian <llewellyn@telluricaquarian.com>
2025-04-01 21:24:57 +1100

    Updated portfolio with latest changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,8 +123,8 @@ export default function Page() {
                   Check out my latest work
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  I&apos;ve worked on a variety of projects, from simple
-                  websites to complex web applications. Here are a few of my
+                  I&apos;ve worked on a variety of projects, mainly simple
+                  websites & brands i've built for myself, Here are a few of my
                   favorites.
                 </p>
               </div>
